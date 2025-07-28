04563e68342c88b894298810b7b291f1ebe9bb44
Shahmeer-create <malikshahmmer@gmail.com>
2025-07-28 18:03:17 +0500

    Create app.js

---
+++ blue/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from BLUE version!');
+});
+
+app.listen(port, () => {
+  console.log(`Blue app listening on port ${port}`);
+});
